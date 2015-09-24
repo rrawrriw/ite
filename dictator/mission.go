b70edfbf7f6490ee2b0a980038b6b4a4e983ee9a
Tim Tochtermann <tim.tochtermann@gmail.com>
2015-09-25 00:14:02 +0200

    add func NewCommandPacket and NewCommandResponsePacket

--- dictator/mission.go
+++ dictator/mission.go
@@ -1,20 +1,41 @@
 package dictator
 
-type MissionSpecs struct {
-	// Funktion sollte eine goroutine starten damit der UDP Listener
-	// weiterhin erreichbach ist.
-	// Die Funktion sollte sich beenden wenn der AppContext Done Channel
-	// geschlossen wird oder wenn der Suicide Channel angesprochen wird.
-	Mission       func(NodeContext)
-	CommandRouter CommandRouter
-	// An diesen Channel werden alle CommandResponse Paket weitergeleitet
-	// Wird im allgemeien in der Mission Funktionen verwendet.
+import "gopkg.in/mgo.v2/bson"
+
+type (
+	MissionSpecs struct {
+		// Funktion sollte eine goroutine starten damit der UDP Listener
+		// weiterhin erreichbach ist.
+		// Die Funktion sollte sich beenden wenn der AppContext Done Channel
+		// geschlossen wird oder wenn der Suicide Channel angesprochen wird.
+		Mission       Mission
+		CommandRouter CommandRouter
+		// An diesen Channel werden alle CommandResponse Paket weitergeleitet
+		// Wird im allgemeien in der Mission Funktionen verwendet.
+		ResponseChan ResponseChan
+	}
+
+	Mission func(NodeContext)
+
+	CommandHandler func(NodeContext, DictatorPayload) error
+
+	CommandRouter map[string]CommandHandler
+
 	ResponseChan chan DictatorPayload
-}
 
-type CommandHandler func(NodeContext) error
+	// Gibt vor welchen Befehl die Nodes ausfürhen sollen
+	CommandBlob struct {
+		Name  string
+		Value interface{}
+	}
 
-type CommandRouter map[string]CommandHandler
+	// Gibt zurück ob Befehl erfolgreich ausgführt wurde
+	CommandResponseBlob struct {
+		NodeID string
+		Status int
+		Result interface{}
+	}
+)
 
 func (r CommandRouter) AddHandler(name string, fun CommandHandler) {
 	r[name] = fun
@@ -27,4 +48,62 @@ func (r CommandRouter) FindHandler(name string) (CommandHandler, bool) {
 	}
 
 	return fun, true
+}
+
+func NewCommandPacket(dictatorID string, cmdName string, cmdValue interface{}) (UDPPacket, error) {
+	commandBlob := CommandBlob{
+		Name:  cmdName,
+		Value: cmdValue,
+	}
+
+	dictatorPayloadBlob, err := bson.Marshal(commandBlob)
+	if err != nil {
+		return UDPPacket{}, nil
+	}
+
+	dictatorPayload := DictatorPayload{
+		Type:       2,
+		DictatorID: dictatorID,
+		Blob:       dictatorPayloadBlob,
+	}
+
+	udpPayload, err := bson.Marshal(dictatorPayload)
+	if err != nil {
+		return UDPPacket{}, nil
+	}
+
+	packet := UDPPacket{
+		Payload: udpPayload,
+	}
+
+	return packet, nil
+
+}
+
+func NewCommandResponsePacket(dictatorID, nodeID string, respStatus int, respResult interface{}) (UDPPacket, error) {
+	commandResponseBlob := CommandResponseBlob{
+		Status: respStatus,
+		Result: respResult,
+		NodeID: nodeID,
+	}
+	commandBlob, err := bson.Marshal(commandResponseBlob)
+	if err != nil {
+		return UDPPacket{}, nil
+	}
+
+	dictatorPayload := DictatorPayload{
+		Type:       3,
+		Blob:       commandBlob,
+		DictatorID: dictatorID,
+	}
+	udpPayload, err := bson.Marshal(dictatorPayload)
+	if err != nil {
+		return UDPPacket{}, nil
+	}
+
+	udpPacket := UDPPacket{
+		Payload: udpPayload,
+	}
+
+	return udpPacket, nil
 }
--- dictator/mission_test.go
+++ dictator/mission_test.go
@@ -1,10 +1,14 @@
 package dictator
 
-import "testing"
+import (
+	"testing"
+
+	"gopkg.in/mgo.v2/bson"
+)
 
 func Test_CommandRouter(t *testing.T) {
 	router := CommandRouter{}
-	h := func(nCtx NodeContext) error {
+	h := func(nCtx NodeContext, p DictatorPayload) error {
 		return nil
 
 	}
@@ -16,10 +20,88 @@ func Test_CommandRouter(t *testing.T) {
 		t.Fatal("Expect to find a handler")
 	}
 
-	result := fun(NodeContext{})
+	result := fun(NodeContext{}, DictatorPayload{})
 
 	if result != nil {
 		t.Fatal(result.Error())
+	}
+
+}
+
+func Test_NewCommandPacket_OK(t *testing.T) {
+	cmdValue := 1
+	cmdName := "test"
+	dictatorID := "1"
+	packet, err := NewCommandPacket(dictatorID, cmdName, cmdValue)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dPayload := DictatorPayload{}
+	err = bson.Unmarshal(packet.Payload, &dPayload)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !IsCommand(dPayload) {
+		t.Fatal("Expect to be Command")
+	}
+
+	if dPayload.DictatorID != dictatorID {
+		t.Fatal("Expect", dictatorID, "was", dPayload.DictatorID)
+	}
+
+	cmdBlob := CommandBlob{}
+	err = bson.Unmarshal(dPayload.Blob, &cmdBlob)
+
+	if cmdBlob.Name != cmdName {
+		t.Fatal("Expect", cmdName, "was", cmdBlob.Name)
+	}
+
+	value := cmdBlob.Value.(int)
+	if value != cmdValue {
+		t.Fatal("Expect", cmdValue, "was", cmdBlob.Value)
+	}
+}
+
+func Test_NewCommandResponsePacket_OK(t *testing.T) {
+	respStatus := 1
+	respResult := 2
+	dictatorID := "1"
+	nodeID := "2"
+	packet, err := NewCommandResponsePacket(dictatorID, nodeID, respStatus, respResult)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dPayload := DictatorPayload{}
+	err = bson.Unmarshal(packet.Payload, &dPayload)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !IsCommandResponse(dPayload) {
+		t.Fatal("Expect to be CommandResponse")
+	}
+
+	cResponseBlob := CommandResponseBlob{}
+	err = bson.Unmarshal(dPayload.Blob, &cResponseBlob)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if cResponseBlob.NodeID != nodeID {
+		t.Fatal("Expect", nodeID, "was", cResponseBlob.NodeID)
+	}
+
+	if cResponseBlob.Status != respStatus {
+		t.Fatal("Expect", respStatus, "was", cResponseBlob.Status)
+	}
+
+	result := cResponseBlob.Result.(int)
+	if result != respResult {
+		t.Fatal("Expect", respResult, "was", result)
 	}
 
 }
